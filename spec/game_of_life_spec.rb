495808bc8bdde5291e6fc7335ea63d467be58eda
Samson Brock <SamsonLB@gmail.com>
2015-10-31 15:26:55 -0600

    Test a cell knows how many neighbors are alive or dead

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe("Game of Life") do
     end
 
     it("knows its x-y coordinates in the universe") do
-      cell = Cell.new(1 , 2)
+      cell = Cell.new(1, 2)
 
       expect(cell.x).to be(1)
       expect(cell.y).to be(2)
@@ -27,6 +27,21 @@ RSpec.describe("Game of Life") do
 
       expect(cell.x).to be(0)
       expect(cell.y).to be(0)
+    end
+
+    it("knows how many of its neighbors are alive or dead") do
+      cell = Cell.new(1, 1)
+      Cell.new(0, 0)
+      Cell.new(1, 0).die
+      Cell.new(2, 0)
+      Cell.new(0, 1)
+      Cell.new(2, 1)
+      Cell.new(0, 2).die
+      Cell.new(1, 2)
+      Cell.new(2, 2).die
+
+      expect(cell.neighbors.alive).to eq(5)
+      expect(cell.neighbors.dead).to eq(3)
     end
   end
 end
