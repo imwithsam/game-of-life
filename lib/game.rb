bd0948647846f00de275ea6f15ca2660e1d63c11
Samson Brock <SamsonLB@gmail.com>
2015-10-31 16:56:33 -0600

    Add Game#nextRound and round attribute

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,12 @@
 class Game
-  attr_reader :grid
+  attr_reader :grid, :round
 
   def initialize(grid)
     @grid = grid
+    @round = 0
+  end
+
+  def nextRound
+    @round = round + 1
   end
 end
