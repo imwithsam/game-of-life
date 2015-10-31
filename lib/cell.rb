a2176cd367bfb16c162b08436b3948681deaaf4c
Samson Brock <SamsonLB@gmail.com>
2015-10-31 15:35:30 -0600

    Add 8 neighboring cells to Cell#neighbors

--- lib/cell.rb
+++ lib/cell.rb
@@ -14,4 +14,27 @@ class Cell
   def die
     @alive = false
   end
+
+  def neighbors
+    neighbors = []
+    cell_nw = Cell.get(x - 1, y - 1)
+    cell_n  = Cell.get(x, y - 1)
+    cell_ne = Cell.get(x + 1, y - 1)
+    cell_w  = Cell.get(x - 1, y)
+    cell_e  = Cell.get(x + 1, y)
+    cell_sw = Cell.get(x - 1, y + 1)
+    cell_s  = Cell.get(x, y + 1)
+    cell_se = Cell.get(x + 1, y + 1)
+
+    neighbors << cell_nw
+    neighbors << cell_n
+    neighbors << cell_ne
+    neighbors << cell_w
+    neighbors << cell_e
+    neighbors << cell_sw
+    neighbors << cell_s
+    neighbors << cell_se
+
+    neighbors
+  end
 end
