c8de9da57a6b70f4f9b703fee5dd41cfb5238666
Samson Brock <SamsonLB@gmail.com>
2015-10-31 16:25:10 -0600

    Pass grid to cell so cell is aware of its neighbors

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,14 @@
+require './lib/grid'
+
 class Cell
   attr_reader :x, :y
 
-  def initialize(x = 0, y = 0)
+  def initialize(grid = nil, x = 0, y = 0)
     @alive = true
+    @grid = grid
     @x = x
     @y = y
+    @grid.add(self) if @grid
   end
 
   def alive?
@@ -13,18 +17,20 @@ class Cell
 
   def die
     @alive = false
+
+    self
   end
 
-  def neighbors(grid)
+  def neighbors
     neighbors = []
-    cell_nw = grid.getCell(x - 1, y - 1)
-    cell_n  = grid.getCell(x, y - 1)
-    cell_ne = grid.getCell(x + 1, y - 1)
-    cell_w  = grid.getCell(x - 1, y)
-    cell_e  = grid.getCell(x + 1, y)
-    cell_sw = grid.getCell(x - 1, y + 1)
-    cell_s  = grid.getCell(x, y + 1)
-    cell_se = grid.getCell(x + 1, y + 1)
+    cell_nw = @grid.getCell(x - 1, y - 1)
+    cell_n  = @grid.getCell(x, y - 1)
+    cell_ne = @grid.getCell(x + 1, y - 1)
+    cell_w  = @grid.getCell(x - 1, y)
+    cell_e  = @grid.getCell(x + 1, y)
+    cell_sw = @grid.getCell(x - 1, y + 1)
+    cell_s  = @grid.getCell(x, y + 1)
+    cell_se = @grid.getCell(x + 1, y + 1)
 
     neighbors << cell_nw
     neighbors << cell_n
